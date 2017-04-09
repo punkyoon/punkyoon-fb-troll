15be203550f1dca68e4e475b0da943976132b513
punkyoon <cryptography@konkuk.ac.kr>
2017-04-09 12:01:37 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 ## 소개
 
-[이상혁](http://www.facebook.com/shlee322)이 페이스북에 글을 쓸 때마다 힘내서 열심히 일을 하라는 격려의 댓글을 달아줘요! \^오^
+[하지윤](http://www.facebook.com/jiyoon96)이 페이스북에 글을 쓸 때마다 힘내서 열심히 일을 하라는 격려의 댓글을 달아줘요!
+
+[shlee322-fb-troll](https://github.com/devxoul/shlee322-fb-troll) 코드를 python3, facebook-sdk의 최신버전에 맞게 고쳤습니다.
 
 ## 사용법
 
 1. `troll.py`를 열어 `token = 'YOUR_ACCESS_TOKEN_HERE'` 부분을 본인의 페이스북 액세스 토큰으로 저장한다.
-2. `$ python troll.py` 하거나 `crontab`을 돌리거나 `celery`를 붙이거나 알아서 잘 쓴다.
-
+2. `$ python troll.py` 혹은 `$ python3 troll.py` 하거나 `crontab`을 돌리거나 `celery`를 붙이거나 알아서 잘 쓴다.
 
 ## 주의사항
 
-1. 중복검사를 위해 최신 글의 아이디만 저장하기 때문에 혹시 상혁이가 그 글을 지우면 이전 글에 또 일하라는 격려 댓글을 남기게 됩니다.
+1. 중복검사를 위해 최신 글의 아이디만 저장하기 때문에 혹시 지윤이가 글을 지우면 이전 글에 또 일하라는 격려 댓글을 남기게 됩니다.
 2. 액세스토큰을 연장하는 로직이 없습니다. 조금 돌리다보면 `GraphAPIError`를 마구 뿜을겁니다.
-3. 테스트를 충분히 하지 않았습니다. (근데 잘 될겁니다 아마도)
+
+## Thanks to..
+
+[shlee322-fb-troll](https://github.com/devxoul/shlee322-fb-troll)
--- troll.py
+++ troll.py
@@ -1,56 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from facebook import GraphAPI as GraphAPIBase
+from facebook import GraphAPI
 from facebook import GraphAPIError
 from json import loads as _parse_json
-import urllib2
-
+import urllib
 
 token = 'YOUR_ACCESS_TOKEN_HERE'
 
-
-class GraphAPI(GraphAPIBase):
-
-    def put_photo_comment(self, image, feed_id, message=None, **kwargs):
-        """Publish photo comment on feed.
-        아나 왜 이걸 안만들어놓은거야 ㅡㅡ
-        put_photo에서 소스 가져옴.
-        """
-        post_args = {
-            'access_token': self.access_token,
-            'file': image,
-            'message': message,
-        }
-        post_args.update(kwargs)
-        content_type, body = self._encode_multipart_form(post_args)
-        req = urllib2.Request(("https://graph.facebook.com/%s/comments" %
-                               feed_id),
-                              data=body)
-        req.add_header('Content-Type', content_type)
-        try:
-            data = urllib2.urlopen(req).read()
-        #For Python 3 use this:
-        #except urllib2.HTTPError as e:
-        except urllib2.HTTPError, e:
-            data = e.read()  # Facebook sends OAuth errors as 400, and urllib2
-                             # throws an exception, we want a GraphAPIError
-        try:
-            response = _parse_json(data)
-            # Raise an error if we got one, but don't not if Facebook just
-            # gave us a Bool value
-            if (response and isinstance(response, dict) and
-                    response.get("error") and
-                    response.get("error")['code'] != 2):
-                raise GraphAPIError(response)
-        except ValueError:
-            response = data
-
-        return response
-
-
 def troll():
-    """ㅋㅋㅋ"""
-
     # 최근 실행에서 저장된 feed id
     fid = None
     try:
@@ -59,29 +16,19 @@ def troll():
         f.close()
     except IOError:
         pass
-    print '* Last feed id:', fid
+    print('* Last feed id:', fid)
 
-    graph = GraphAPI(token)
+    graph = GraphAPI(access_token=token)
 
-    print '* Retrieving feeds...',
-    feeds = graph.get_connections('shlee322', 'feed',
-                                  fields='message,from.id')['data']
-    print 'Done'
-
-    if len(feeds):
-        print '* Reading image.jpg...',
-        img = open('image.jpg')
-        print 'Done'
+    print('* Retrieving feeds...')
+    feeds = graph.get_connections(id='674366342659988', connection_name='feed', fields='message,id')['data']
+    print('Done')
 
     # 받아온 피드 중 가장 최신 피드의 id
     lastest_feed_id = None
     for feed in feeds:
-        # 본인이 직접 작성한 글이 아니면 트롤링 ㄴㄴ
-        if 'message' not in feed or feed['from']['id'] != '100001582221312':
-            continue
-
-        # feed id가 unicode로 들어와서 encode 해줘야됨
-        feed_id = feed['id'].encode('utf-8')
+        feed_id = feed['id']
+        feed_id=str(feed_id)
 
         # 받아온 피드 중 가장 최신의 피드 id 저장
         if lastest_feed_id is None:
@@ -91,20 +38,20 @@ def troll():
         if fid == feed_id:
             break
 
-        print '* Publishing comment on %s...' % feed_id,
-        img.seek(0)
+        print('* Publishing comment on %s...' % feed_id)
         try:
-            graph.put_photo_comment(img, feed_id, message='일 안함? ㅡㅡ')
-        except GraphAPIError:
-            print 'Failed'
+            graph.put_comment(object_id=feed_id, message='일 안함? ㅡㅡ')
+        except GraphAPIError as e:
+            print(e)
+            print('Failed')
             continue
-        print 'Done'
+        print('Done')
 
     # 중복검사를 위해 가장 최신 피드 id 저장
     f = open('fid.txt', 'w')
     f.write(lastest_feed_id)
     f.close()
-    print '* Finished.'
+    print('* Finished.')
 
 if __name__ == '__main__':
     troll()
